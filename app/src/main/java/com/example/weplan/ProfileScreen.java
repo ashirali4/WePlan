3be69f49b3c2ee880db7debf5ca730b6364cfa34
saleem <saleemarshad001@gmail.com>
2020-03-05 21:44:21 +0500

    New Changes1

--- app/src/main/java/com/example/weplan/ProfileScreen.java
+++ app/src/main/java/com/example/weplan/ProfileScreen.java
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProfileScreen extends AppCompatActivity {
 
 
     TextView textName,textType,textPrice,textLocation;
-    Bitmap image;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +22,19 @@ public class ProfileScreen extends AppCompatActivity {
 
         Intent intent=getIntent();
         Bundle bundle=intent.getExtras();
-//        String name=bundle.getString("name");
-//        String location=bundle.getString("location");
-//        String rating=bundle.getString("rating");
-//        String budget=bundle.getString("budget");
-//        Bitmap image=bundle.getParcelable("image");
+        String name=bundle.getString("name");
+        String location=bundle.getString("location");
+        String rating=bundle.getString("rating");
+        String budget=bundle.getString("budget");
+        Bitmap bitimage=bundle.getParcelable("image");
 
+        textName=findViewById(R.id.name);
+        textType=findViewById(R.id.serviceType);
+        textPrice=findViewById(R.id.price);
+        textLocation=findViewById(R.id.location);
+        image=findViewById(R.id.profileimage);
+
+        textName.setText(name);
 
 
     }
